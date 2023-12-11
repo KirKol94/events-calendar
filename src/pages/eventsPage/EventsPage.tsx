0013f56577b078e567de5273481aaf3088312b87
kirkol <kirkol94@ya.ru>
2023-12-12 00:53:43 +0500

    сортировка событий по возврастанию

--- src/pages/eventsPage/EventsPage.tsx
+++ src/pages/eventsPage/EventsPage.tsx
@@ -2,21 +2,28 @@ import { Container } from "@/components/container";
 import { EventsFilter } from "@/components/eventsFilter";
 import { useEventsStore } from "@/store/events.store";
 import { useEventsFilterStore } from "@/store/eventsFilter.store";
+import { dateConverter } from "@/utils/dateConverter";
 import { useEffect } from "react";
 
 const EventsPage = () => {
   const searchValue = useEventsFilterStore((state) => state.searchValue);
   const events = useEventsStore((state) =>
-    state.events.filter((e) =>
-      e.title.toLocaleLowerCase().includes(searchValue.toLowerCase())
-    )
+    state.events
+      .filter((e) =>
+        e.title.toLocaleLowerCase().includes(searchValue.toLowerCase())
+      )
+      .sort(
+        (a, b) =>
+          +new Date(dateConverter(a.date_start)) -
+          +new Date(dateConverter(b.date_start))
+      )
   );
   const isLoading = useEventsStore((state) => state.isLoading);
   const fetchEvents = useEventsStore((state) => state.fetchEvents);
 
   useEffect(() => {
     fetchEvents();
-  }, []);
+  }, [fetchEvents]);
 
   if (isLoading) {
     return " Loading...";
@@ -31,50 +38,27 @@ const EventsPage = () => {
           <table className="min-w-full table-auto border-collapse">
             <thead className="bg-gray-200">
               <tr>
-                <th className="border border-gray-400 px-4 py-2">Title</th>
-                <th className="border border-gray-400 px-4 py-2">Start Date</th>
-                <th className="border border-gray-400 px-4 py-2">End Date</th>
-                <th className="border border-gray-400 px-4 py-2">Location</th>
-                <th className="border border-gray-400 px-4 py-2">
-                  Description
-                </th>
-                <th className="border border-gray-400 px-4 py-2">URL</th>
-                <th className="border border-gray-400 px-4 py-2">
-                  Ticket Price
-                </th>
+                <th className="px-4 py-2">Date</th>
+                <th className="px-4 py-2">Event Name</th>
+                <th className="px-4 py-2">Location</th>
               </tr>
             </thead>
             <tbody>
               {events.map((event, index) => (
                 <tr key={index}>
-                  <td className="border border-gray-400 px-4 py-2">
-                    {event.title}
+                  <td className="px-4 py-2">
+                    <span className="p-2 bg-red-500">
+                      {`${event.date_start.split("/")[1]}-${
+                        event.date_end !== event.date_start &&
+                        event.date_end?.split("/")[1]
+                      }`}
+                    </span>
+                    {new Date(dateConverter(event.date_start)).getMonth() + 1}
+                    {"-"}
+                    {new Date(dateConverter(event.date_start)).getFullYear()}
                   </td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    {event.date_start}
-                  </td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    {event.date_end}
-                  </td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    {event.location}
-                  </td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    {event.description}
-                  </td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    <a
-                      href={event.url}
-                      className="text-blue-500 underline"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {event.url}
-                    </a>
-                  </td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    {event.ticket_price}
-                  </td>
+                  <td className="px-4 py-2">{event.title}</td>
+                  <td className="px-4 py-2">{event.location}</td>
                 </tr>
               ))}
             </tbody>
